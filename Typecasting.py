4bc28978226e7243ac3a6d92b41c1587e4641fc5
Jishnu-Karthik <jishnukarthikkn@gmail.com>
2026-05-21 18:38:35 +0530

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,27 @@
+name = "Jishnu"
+age = 13
+weight = 50.4
+is_student: true
+
+print("Name: ", name)
+print("\n Data type of name is, ", type(name)
+
+print("\n Age: ", age)
+print("Data type of age is, ", type(age)
+
+print("\n Weight: ", weight)
+print("\n Data type of weight is, ", type(weight)
+
+print("\n Is_student: ", is_student)
+print("\n Data type of is_student is, ", type(is_student)
+
+print("\n After typecasting")
+
+age = str(age)
+weight = int(weight)
+
+print("\n Age: ", age)
+print("\n Data type of age is: ", type(age))
+
+print("\n Weight: ", weight)
+print("\n Data type of weight is: ", type(weight))
