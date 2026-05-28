be26800617ac5f589e37f79eeabe960a5e136186
Jishnu-Karthik <jishnukarthikkn@gmail.com>
2026-05-28 12:45:49 +0530

    Add files via upload

---
+++ Bday.py
@@ -0,0 +1,13 @@
+from datetime import date 
+
+Bday1 = date(1983, 2, 25)
+Bday2 = date(1985, 2, 25)
+Bday3 = date(2012, 12, 7)
+Bday4 = date(2018, 2, 7)
+Bday5 = date(1967, 6, 7)
+
+print("Birthday number 1:", Bday1)
+print("Birthday number 2:", Bday2)
+print("Birthday number 3:", Bday3)
+print("Birthday number 4:", Bday4)
+print("Birthday number 5:", Bday5)
