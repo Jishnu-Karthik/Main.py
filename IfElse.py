4d23f565272c8e60e9b195f3f907557fc5733259
Jishnu-Karthik <jishnukarthikkn@gmail.com>
2026-06-01 18:36:40 +0530

    Add files via upload

---
+++ IfElse.py
@@ -0,0 +1,8 @@
+i = int(input("Enter a number here:"))
+if (i < 15):
+	print("i is smaller than 15")
+	print("I am in the if statement")
+else:
+	print("i is greater than 15")
+	print("I am in the else statement")
+print("I'm not in the if or else statement")
